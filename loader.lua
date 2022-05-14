67005d28403995c4c94061e48415194f58053559
tomvere <66671065+tomvere@users.noreply.github.com>
2022-05-14 14:18:36 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId = 662417684 then
-    
+    loadstring(game.HttpService:GetAsync(("https://raw.githubusercontent.com/tomvere/MaxHub/main/Lucky%20Block_JiuyH45Hdzd45?token=GHSAT0AAAAAABUR6WFJMGVOMOAKSTEBQWNIYT7TZIQ")))
 end
